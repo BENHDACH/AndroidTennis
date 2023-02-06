cba11c1e308efce8f47aa934a6c00c96c7ed576f
aliciane-pinault <aliciane.pinault@isen.yncrea.fr>
2023-02-06 16:21:40 +0100

    lien between accueil et chat

--- app/src/main/java/com/example/whykotlin/ChatActivity.kt
+++ app/src/main/java/com/example/whykotlin/ChatActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ChatActivity : AppCompatActivity() {
+
+    companion object{
+        val extraKey = "extraKey"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
