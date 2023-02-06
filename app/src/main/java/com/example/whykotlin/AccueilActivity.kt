028a05e36e64685c397b1f5ac4f23339ef8465e7
unknown <MathildeH-28>
2023-02-06 13:18:44 +0100

    ajout chat

--- app/src/main/java/com/example/whykotlin/AccueilActivity.kt
+++ app/src/main/java/com/example/whykotlin/AccueilActivity.kt
@@ -3,7 +3,7 @@ package com.example.whykotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Accueil : AppCompatActivity() {
+class AccueilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accueil)
