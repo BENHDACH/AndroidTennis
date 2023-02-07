d087aca7472a5125d4095d5e82ca2331a22c47f4
BENHDACH <mohamed.benhdach@isen.yncrea.fr>
2023-02-07 14:58:19 +0100

    ajout cal 3

--- app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
+++ app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
@@ -3,6 +3,7 @@ package com.example.whykotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.whykotlin.databinding.ActivityClendrierBinding
 
@@ -24,6 +25,8 @@ class ClendrierActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = GridLayoutManager(this, 8)
         binding.recyclerView.adapter = Adapter() { weekday, hour ->
             val intent = Intent(this, ReservationActivity::class.java)
+            intent.putExtra("Heure","${hour+6}")
+            intent.putExtra("Jour","${weekday}")
             startActivity(intent)
         }
 
