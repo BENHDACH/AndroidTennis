9dfb500fbf9c995f3f83ed557b9d8756b294a9fa
aliciane-pinault <aliciane.pinault@isen.yncrea.fr>
2023-02-07 14:13:05 +0100

    ajout des text input layout, un peu de desing et des tentatives pour recupe les données saisi au clavier

--- app/src/main/java/com/example/whykotlin/AjoutadhActivity.kt
+++ app/src/main/java/com/example/whykotlin/AjoutadhActivity.kt
@@ -1,8 +1,11 @@
 package com.example.whykotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.example.whykotlin.databinding.ActivityAccueilBinding
 import com.example.whykotlin.databinding.ActivityAjoutadhBinding
@@ -20,16 +23,41 @@ class AjoutadhActivity : AppCompatActivity() {
         setContentView(binding.root)
         //setContentView(R.layout.activity_ajoutadh)
 
-        //buttonsListener()
+        buttonsListener()
+
+        //modifier pour avoir les données rentrer au clavier
+
+        writeNewUser("M","MBS","J.hotmail")
     }
 
-    /*private fun buttonsListener() {
+
+    fun writeNewUser(userId: String, userPsw: String, userRk: String) {
+        val user = User(userPsw, userRk)
+
+        Data.database.child("users").child(userId).setValue(user)
+    }
+
+
+    private fun buttonsListener() {
 
             binding.newname.setOnClickListener {
-                Toast.makeText(this, "ajouter le nom d'un nel adhérant", Toast.LENGTH_LONG).show()
-                binding.sname.visibility = View.GONE
+                Log.d("textnewname", "Click sur newname")
+
             }
-    }*/
+
+        /*if ( == null){
+            //ne pas enregistrer et afficher un message
+            Toast.makeText(this, "Données non valide", Toast.LENGTH_LONG).show()
+        }*/
+
+        //if enregistrement bon
+        binding.enr.setOnClickListener{
+            val intent = Intent(this, AccueilActivity::class.java)
+            startActivity(intent)
+        }
+        //else adhérant déjà existant
+        //else données rentrer invalide
+    }
 
 }
 
