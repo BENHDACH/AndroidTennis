ff414a398bba8fe2e372006e51edcff6c3431035
aliciane-pinault <aliciane.pinault@isen.yncrea.fr>
2023-02-07 11:11:45 +0100

    before pull aliciane

--- app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
+++ app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
@@ -7,13 +7,13 @@ import com.example.whykotlin.databinding.ActivityMainBinding
 
 class ClendrierActivity : AppCompatActivity() {
 
-<<<<<<< HEAD
+
     private lateinit var binding: ActivityClendrierBinding
-=======
+
     companion object{
         val extraKeyr = "extraKeyr"
     }
->>>>>>> 6a94a9077759f3e39d8d60f98acc1f2e831cf18c
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clendrier)
