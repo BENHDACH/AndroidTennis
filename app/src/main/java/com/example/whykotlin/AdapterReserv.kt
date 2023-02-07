63dba68eb5265603106b75ceb220f5802897f592
unknown <MathildeH-28>
2023-02-07 13:59:25 +0100

    adpater pour la liste des gens pour la réservation

---
+++ app/src/main/java/com/example/whykotlin/AdapterReserv.kt
@@ -0,0 +1,15 @@
+package layout
+
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterReserv {
+
+    /*class CellViewHolder(binding: CellCustomBinding) : RecyclerView.ViewHolder(binding.root) {
+        val textView : TextView = binding.itemName
+        val imageView = binding.imageView
+        val priceTextView = binding.priceTextView
+        val root: ConstraintLayout = binding.root
+    }*/
+}
