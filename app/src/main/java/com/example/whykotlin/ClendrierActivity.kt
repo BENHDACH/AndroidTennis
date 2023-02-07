5ef36adade903a406c0a2ed0b57138ca7342d5be
BENHDACH <mohamed.benhdach@isen.yncrea.fr>
2023-02-07 09:00:43 +0100

    before pull R

--- app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
+++ app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
@@ -2,10 +2,17 @@ package com.example.whykotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.whykotlin.databinding.ActivityClendrierBinding
+import com.example.whykotlin.databinding.ActivityMainBinding
 
 class ClendrierActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityClendrierBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clendrier)
+
+        binding = ActivityClendrierBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
