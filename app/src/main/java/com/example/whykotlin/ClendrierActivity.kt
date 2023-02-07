b1a95a1dbb959153424e3d2048226b6e57bc1203
aliciane-pinault <aliciane.pinault@isen.yncrea.fr>
2023-02-07 08:19:06 +0100

    coorection des liens depuis la pages acueil vers les réservations terrains 1 et 2 et vers le tchat

--- app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
+++ app/src/main/java/com/example/whykotlin/ClendrierActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ClendrierActivity : AppCompatActivity() {
+
+    companion object{
+        val extraKeyr = "extraKeyr"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clendrier)
